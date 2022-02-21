69dd7c0be45d4703018d100d4b307591ac467e53
Lehlohonolo Letaoana <lehlohonolo.letaoana@gmail.com>
2022-02-21 20:31:11 +0200

    Added solution

---
+++ LetterCapitalize/LetterCapitalize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LetterCapitalize
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(LetterCapitalize("hello world!"));
+        }
+
+        public static string LetterCapitalize(string str)
+        {
+            var wordArray = str.Split(' ');
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                var word = wordArray[i].ToCharArray();
+                word.SetValue(char.Parse(word.FirstOrDefault().ToString().ToUpper()), 0);
+                wordArray[i] = new string(word);
+            }
+            return string.Join(" ", wordArray);
+        }
+    }
+}
